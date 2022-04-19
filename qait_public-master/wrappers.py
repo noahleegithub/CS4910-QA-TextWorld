6798cabe2998af3ba44572975e979fe75c8ce681
Noah Lee <noah.v.lee@gmail.com>
2022-04-19 04:43:24 +0000

    implemented location question reward

--- qait_public-master/wrappers.py
+++ qait_public-master/wrappers.py
@@ -1,10 +1,11 @@
 from typing import Tuple, Any, Mapping, List
 from types import SimpleNamespace
 import gym
-from textworld import GameState
+from textworld import GameState, Game
 from gym.core import Wrapper
 import spacy
 import re
+import numpy as np
 
 from game_generator import generate_qa_pairs
 
@@ -68,17 +69,66 @@ class TokenizerWrapper(gym.ObservationWrapper):
 
     
 class RewardWrapper(gym.Wrapper):
-    # Probably should just make this a Wrapper and override step, reset
-    def __init__(self, env):
+    def __init__(self, env: gym.Env, config: SimpleNamespace):
         super().__init__(env)
+        self.config = config
     
     def reset(self):
-        state, infos = super().reset()
-        return state, infos
+        states, infos = super().reset()
+        self.observation_history = {}
+        self.init_facts = {}
+        for i, state in enumerate(states):
+            self.observation_history[i] = {}
+            self.observation_history[i][" ".join(state)] = True
+            self.init_facts[i] = set(infos['facts'][i])
+        return states, infos
 
     def step(self, commands):
-        state, rewards, done, infos = super().step(commands)
-
+        states, rewards, done, infos = super().step(commands)
+        rewards = np.array(rewards)
+        rewards += self.reward_episodic_discovery(states, infos)
+        if self.config.general.question_type == "location":
+            rewards += self.reward_location(infos)
+        elif self.config.general.question_type == "existence":
+            rewards
+        elif self.config.general.question_type == "attribute":
+            rewards
+        else:
+            raise NotImplementedError
         # episodic discovery reward: 1 for a new state
+        # Sufficient info bonus
+        # - Location: 1 if final state contains object, else 0
+        # - Existence: If answer is yes, use location bonus, else use exploration coverage bonus
+        # - Attribute: Heuristic bonus + 0.1 if entity was observed in any state + 0.1 exploration coverage bonus
+        for i, state in enumerate(states):
+            self.observation_history[i][" ".join(state)] = True
 
-        return state, rewards, done, infos
+        return states, rewards, done, infos
+
+    def reward_episodic_discovery(self, states, infos):
+        rewards = np.zeros(len(states))
+        for i, state in enumerate(states):
+            if self.observation_history[i][" ".join(state)]:
+                rewards[i] = 1.
+            else:
+                rewards[i] = 0.
+        return rewards
+
+    def reward_location(self, infos):
+        rewards = np.zeros(len(self.observation_history))
+        for sample_idx, history in self.observation_history.items():
+            entity = infos['reward_info']["_entities"][sample_idx]
+            if entity in list(history.keys())[-1]:
+                rewards[sample_idx] = 1.
+            else:
+                rewards[sample_idx] = 0.
+        return rewards
+
+    def reward_exploration_coverage(self, infos):
+        for i in range(len(self.init_facts)):
+            game = Game.deserialize(infos['game'][i])
+            all_facts = set(game.world.facts)
+            current_facts = set(infos['facts'][i]) # rework this; should be union of all discovered facts
+            initial_facts = self.init_facts[i]
+        pass
+    
